f8d45a8c0187ab05b191d8703a059d57f12ef981
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-23 05:08:59 -0200

    Disponibilizando o index atraves de GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :customers
-  resources :welcome
+  #resources :welcome
+  GET 'inicio' => 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
