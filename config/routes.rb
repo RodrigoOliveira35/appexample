13718611f376992e6464394de255b97882d2eb04
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-24 19:52:46 -0200

    Alteração do arquivo routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #resources :welcome
   get 'inicio' => 'welcome#index'
   get 'presentation' => 'presentation#index'
+  get 'teste' => 'teste#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
