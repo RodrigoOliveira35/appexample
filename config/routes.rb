68a9014c24a1c732be653ffa383a321a9afa5142
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-24 19:49:44 -0200

    Alteração da rota

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :customers
   #resources :welcome
   get 'inicio' => 'welcome#index'
-  get 'presentantion' => 'presentation#index'
+  get 'presentation' => 'presentation#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
