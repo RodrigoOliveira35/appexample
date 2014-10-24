e55057fa9e345628793402edaabc0c4752989400
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-24 21:18:03 -0200

    Alteração dos arquivos

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022072815) do
-
-  create_table "customers", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.date     "birthday"
-    t.text     "obs"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
